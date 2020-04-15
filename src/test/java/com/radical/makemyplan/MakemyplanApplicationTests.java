7bcaf79d5ddddb4313cba7aeee83eb4287a9d79b
rahuljidevops <rahulnandanwar100@gmail.com>
2020-04-15 17:15:16 +0530

    coderefactor

---
+++ src/test/java/com/radical/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radical.makemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
